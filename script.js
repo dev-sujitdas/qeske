42db467f3abf14dc1380ea8c65020c719390cbbc
dev-sujitdas <dev.sujitdas@gmail.com>
2025-07-18 21:45:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+
+Shery.imageEffect(".images", {
+  style: 5,
+  config: {
+    a: { value: 1.15, range: [0, 30] },
+    b: { value: -0.98, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 2.131121642969984 },
+    ignoreShapeAspect: { value: true },
+    shapePosition: { value: { x: 0, y: 0 } },
+    shapeScale: { value: { x: 0.5, y: 0.5 } },
+    shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+    shapeRadius: { value: 0, range: [0, 2] },
+    currentScroll: { value: 0 },
+    scrollLerp: { value: 0.07 },
+    gooey: { value: false },
+    infiniteGooey: { value: true },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: false },
+    maskVal: { value: 1, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2], _gsap: { id: 1 } },
+    discard_threshold: { value: 0.48, range: [0, 1] },
+    antialias_threshold: { value: 0.04, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+  gooey: true,
+});
+
+
+ 
+function cursorAnimation() {
+
+  let videoContainer = document.querySelector(".video-container");
+  let video = document.querySelector(".video-container video")
+  videoContainer.addEventListener("mouseenter", function () {
+    videoContainer.addEventListener("mousemove", function (dets) {
+      gsap.to(".mousefollower", {
+        opacity: 0
+      });
+      gsap.to("#video-cursor", {
+        left: dets.x - 500,
+        y: dets.y - 200,
+      });
+    });
+  });
+  videoContainer.addEventListener("mouseleave", function () {
+    gsap.to(".mousefollower", {
+      opacity: 1
+    });
+    gsap.to("#video-cursor", {
+      left: "-5%",
+      top: "5%",
+    });
+  });
+
+
+  let flag = 0
+  videoContainer.addEventListener("click", function () {
+    if (flag == 0) {
+      video.play()
+      video.style.opacity = 1
+      document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`
+      gsap.to("#video-cursor", {
+        scale: 0.5
+      })
+      flag = 1
+    } else {
+      video.pause()
+      video.style.opacity = 0
+      document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`
+      gsap.to("#video-cursor", {
+        scale: 1
+      })
+      flag = 0
+    }
+  })
+}
+
+cursorAnimation();
